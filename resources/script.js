df3953a5a1856e73422e03fd3c918df4160e3a0d
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-08-20 20:52:55 +0000

    Added Munir to names.txt and the theme-toogle functionality

--- resources/script.js
+++ resources/script.js
@@ -1,15 +1,26 @@
 // Parse content of text file
 let xmlhttp;
-if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-} else {// code for IE6, IE5
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+if (window.XMLHttpRequest) {
+  // code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp = new XMLHttpRequest();
+} else {
+  // code for IE6, IE5
+  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        let allNames = document.getElementById('names-list');
-        allNames.innerText = (xmlhttp.responseText);
-    }
-}
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    let allNames = document.getElementById("names-list");
+    allNames.innerText = xmlhttp.responseText;
+  }
+};
 xmlhttp.open("GET", "names.txt", true);
 xmlhttp.send();
+
+// THEME TOGGLE
+
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("light");
+  document.querySelector("#headline").classList.toggle("light");
+  document.querySelector("#reload-btn").classList.toggle("light");
+});
