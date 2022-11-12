114c6ee0b5766ba3b02e56392b6e1e8711a00915
Patrick Aziken <onaziken@gmail.com>
2022-11-12 23:40:53 +0100

    Switch to Fetch API form XMLHttp Request - Now fetching names from file using Fetch API

--- resources/script.js
+++ resources/script.js
@@ -1,24 +1,17 @@
 // Parse content of text file
-let xmlhttp;
-if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-} else {// code for IE6, IE5
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+fetchNames("names.txt");
+
+async function fetchNames(file) {
+    let fetchFile = await fetch(file);
+    let spoolName = await fetchFile.text();
+    document.getElementById("names-list").innerText = spoolName;
 }
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        let allNames = document.getElementById('names-list');
-        allNames.innerText = (xmlhttp.responseText);
-    }
-}
-xmlhttp.open("GET", "names.txt", true);
-xmlhttp.send();
 
 // THEME TOGGLE
 
 const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("light");
-  document.querySelector("#headline").classList.toggle("light");
-  document.querySelector("#reload-btn").classList.toggle("light");
+    document.body.classList.toggle("light");
+    document.querySelector("#headline").classList.toggle("light");
+    document.querySelector("#reload-btn").classList.toggle("light");
 });
